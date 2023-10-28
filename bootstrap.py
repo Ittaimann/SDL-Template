47cd5708e607628506477fb9ad4480253d119003
Ittai Mann <ittaiam@gmail.com>
2023-10-28 02:29:48 +0100

    updated some incorrect things with the bootstrap regarding hardware acceleration and window shown by default

--- bootstrap.py
+++ bootstrap.py
@@ -46,10 +46,10 @@ int main(int argc, char *argv[])
     SDL_WINDOWPOS_UNDEFINED,
     640,
     480,
-    0
+    SDL_WINDOW_SHOWN
   );
 
-  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
   SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
   SDL_RenderClear(renderer);
   SDL_RenderPresent(renderer);
